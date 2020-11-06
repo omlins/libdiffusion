c40e5d6ae3b013551b8932382ca497a969495868
omlins <samuel.omlin@cscs.ch>
2020-11-06 18:25:01 +0100

    Update build/build.jl
    make path more portable
    
    Co-authored-by: Kristoffer Carlsson <kcarlsson89@gmail.com>

--- build/build.jl
+++ build/build.jl
@@ -4,6 +4,6 @@ PackageCompiler.create_sysimage(Symbol[:Diffusion];
                                 project=joinpath(@__DIR__, ".."),
                                 precompile_execution_file=[joinpath(@__DIR__, "generate_precompile.jl")],
                                 precompile_statements_file=[joinpath(@__DIR__, "additional_precompile.jl")],
-                                sysimage_path="libdiffusion.$(Libdl.dlext)",
+                                sysimage_path=joinpath(@__DIR__, "..", "libdiffusion.$(Libdl.dlext)"),
                                 incremental=false,
                                 filter_stdlibs=true)
